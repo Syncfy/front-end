9cd806e1ae1e5d033bba5f68cea82f2e087df417
shimarrudz <vic.shima.vss@gmail.com>
2023-10-09 19:58:24 -0300

    create home screen

---
+++ Syncfy/src/routes/screens/Home/index.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native-reanimated/lib/typescript/Animated';
+import React from 'react';
+
+const Home = () => {
+  return (
+    <View style={{marginTop: 60}}>
+      <Text style={{textAlign: 'center'}}>Home</Text>
+    </View>
+  );
+};
+
+export default Home;
