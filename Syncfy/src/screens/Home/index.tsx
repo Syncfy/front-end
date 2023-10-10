d278284c7ca9159c388123e0263ba6197632a19d
shimarrudz <vic.shima.vss@gmail.com>
2023-10-09 21:12:47 -0300

    fix: text

--- Syncfy/src/screens/Home/index.tsx
+++ Syncfy/src/screens/Home/index.tsx
@@ -1,21 +1,63 @@
-import {View, Text, Button} from 'react-native';
 import React from 'react';
-import {useNavigation} from '@react-navigation/native';
-import {StackTypes} from '../../routes/stack';
+import styled from 'styled-components/native';
 
-const Home = () => {
-  const navigation = useNavigation<StackTypes>();
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff; /* Cor de fundo branca */
+`;
 
+const Logo = styled.Image`
+  width: 200px;
+  height: 200px;
+  margin-top: 50px;
+`;
+
+const WelcomeText = styled.Text`
+  font-size: 32px;
+  font-weight: bold;
+  margin-top: 20px;
+  text-align: center;
+  color: #000000;
+`;
+
+const SubText = styled.Text`
+  font-size: 20px;
+  color: #555; /* Cor do texto menor */
+  margin-top: 10px; /* Espaçamento superior para o texto menor */
+  text-align: center;
+`;
+
+const LoginButton = styled.TouchableOpacity`
+  width: 80%;
+  height: 60px;
+  background-color: #820ad1;
+  border-radius: 20px;
+  justify-content: center;
+  align-items: center;
+  margin-top: 40px; /* Espaçamento superior para o botão de login */
+`;
+
+const ButtonText = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+  color: #fff; /* Cor do texto do botão */
+`;
+
+const Home = ({ navigation }) => {
   return (
-    <View style={{marginTop: 60}}>
-      <Text style={{textAlign: 'center'}}>Home</Text>
-      <Button
-        title="Fazer login"
+    <Container>
+      <Logo source={require('../../assets/images/logo.png')} />
+      <WelcomeText>Seja bem-vindo!</WelcomeText>
+      <SubText>Procedência e segurança é com a gente.</SubText>
+      <LoginButton
         onPress={() => {
           navigation.navigate('Login');
-        }}
-      />
-    </View>
+        }}>
+        <ButtonText>Fazer login</ButtonText>
+      </LoginButton>
+    </Container>
   );
 };
 
