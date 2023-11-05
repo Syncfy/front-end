cda14af29bed080c5e79b9593deeeffbe464e758
shimarrudz <vic.shima.vss@gmail.com>
2023-11-05 00:34:27 -0300

    refactor: modularizing tab navigator

---
+++ Syncfy/src/components/TabNavigator/style.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components/native';
+
+export const tabBarOptions = {
+  tabBarStyle: {
+    backgroundColor: '#fff',
+    padding: 5,
+    height: 60,
+  },
+  tabBarLabelStyle: {
+    fontSize: 12,
+  },
+  tabBarActiveTintColor: '#e91e63',
+  tabBarInactiveTintColor: 'gray',
+};
+
+export const IconImage = styled.Image`
+  width: 30px;
+  height: 30px;
+`;
--- Syncfy/src/routes/stack.tsx
+++ Syncfy/src/routes/stack.tsx
@@ -7,7 +7,7 @@ import {
 import Home from '../screens/Home';
 import Login from '../screens/Login';
 import Register from '../screens/Register';
-import TabNavigator from './tab-navigator';
+import TabNavigator from '../components/TabNavigator/tab-navigator';
 
 const Stack = createNativeStackNavigator();
 
