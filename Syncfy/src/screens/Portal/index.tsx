6cd07fd253ffd548c7b8fa35c25e3c1e8baaa5d7
shimarrudz <vic.shima.vss@gmail.com>
2023-11-04 22:26:33 -0300

    feat: add search icon

--- Syncfy/src/screens/Portal/index.tsx
+++ Syncfy/src/screens/Portal/index.tsx
@@ -1,56 +1,26 @@
-// Portal.tsx
 import React, {useState} from 'react';
-import {SafeAreaView, TextInput, StyleSheet, View} from 'react-native';
+import {Container, SearchContainer, SearchInput} from './style'; // Import styled components
 
 const Portal: React.FC = () => {
   const [searchQuery, setSearchQuery] = useState('');
 
   const handleSearchChange = (query: string) => {
     setSearchQuery(query);
-    // Aqui você pode adicionar a lógica para buscar ou filtrar os dados com base no texto inserido
+    // Logic to handle search
   };
 
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.searchContainer}>
-        <TextInput
-          style={styles.searchInput}
+    <Container>
+      <SearchContainer>
+        <SearchInput
           value={searchQuery}
           onChangeText={handleSearchChange}
           placeholder="Buscar um produto no Syncfy"
           clearButtonMode="while-editing"
         />
-      </View>
-    </SafeAreaView>
+      </SearchContainer>
+    </Container>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  searchContainer: {
-    padding: 10,
-    backgroundColor: '#f8f8f8',
-    borderBottomWidth: 1,
-    borderColor: '#e4e4e4',
-  },
-  searchInput: {
-    backgroundColor: '#ffffff',
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    borderRadius: 20,
-    fontSize: 16,
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.1,
-    shadowRadius: 2,
-    elevation: 2,
-  },
-});
 
 export default Portal;
--- Syncfy/src/screens/Portal/style.tsx
+++ Syncfy/src/screens/Portal/style.tsx
@@ -1,29 +1,26 @@
-import {StyleSheet} from 'react-native';
+import styled from 'styled-components/native';
 
-export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  searchContainer: {
-    padding: 10,
-    backgroundColor: '#f8f8f8',
-    borderBottomWidth: 1,
-    borderColor: '#e4e4e4',
-  },
-  searchInput: {
-    backgroundColor: '#ffffff',
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    borderRadius: 20,
-    fontSize: 16,
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.1,
-    shadowRadius: 2,
-    elevation: 2,
-  },
-});
+export const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+`;
+
+export const SearchContainer = styled.View`
+  padding: 20px;
+  background-color: #52d974;
+  border-bottom-width: 1px;
+  border-color: #e4e4e4;
+`;
+
+export const SearchInput = styled.TextInput`
+  background-color: #ffffff;
+  padding-horizontal: 10px;
+  padding-vertical: 5px;
+  border-radius: 22px;
+  font-size: 16px;
+  shadow-color: #000;
+  shadow-offset: 0px 2px;
+  shadow-opacity: 0.1;
+  shadow-radius: 2px;
+  elevation: 2;
+`;
