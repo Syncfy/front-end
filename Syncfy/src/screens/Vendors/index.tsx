e3f53ac4d61722d099c9c40809beb13afcfafeb2
shimarrudz <vic.shima.vss@gmail.com>
2023-11-04 23:06:45 -0300

    feat: add vendors screen

---
+++ Syncfy/src/screens/Vendors/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Vendors = () => (
+  <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <Text>Fornecedores</Text>
+  </View>
+);
+
+export default Vendors;
