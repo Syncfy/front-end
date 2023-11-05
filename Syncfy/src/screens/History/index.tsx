082b9ba5d90187146b80f33ca917c08593b2e051
shimarrudz <vic.shima.vss@gmail.com>
2023-11-04 23:07:11 -0300

    feat: add history screen

---
+++ Syncfy/src/screens/History/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const History = () => (
+  <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <Text>Histórico</Text>
+  </View>
+);
+
+export default History;
