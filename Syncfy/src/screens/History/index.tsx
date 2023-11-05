e3c36feecb99c145f1deb21e263fda5f5e7dea28
shimarrudz <vic.shima.vss@gmail.com>
2023-11-05 02:13:50 -0300

    feat: add transactions on history page

--- Syncfy/src/screens/History/index.tsx
+++ Syncfy/src/screens/History/index.tsx
@@ -5,17 +5,31 @@ import HistoryCard from '../../components/HistoryCard';
 
 const historyData = [
   {
-    number: '001',
-    value: 1500.0,
-    quantity: 10,
-    vendor: 'Fornecedor A',
+    number: '3125354',
+    value: 1532.59,
+    quantity: 48,
+    vendor: 'Mercado Livre',
     date: '10/01/2023',
   },
   {
-    number: '002',
-    value: 2500.0,
-    quantity: 20,
-    vendor: 'Fornecedor B',
+    number: '35846121',
+    value: 2515.84,
+    quantity: 15,
+    vendor: 'Kabuum',
+    date: '12/01/2023',
+  },
+  {
+    number: '685146351',
+    value: 3215.45,
+    quantity: 4,
+    vendor: 'Amazon',
+    date: '12/01/2023',
+  },
+  {
+    number: '8798456321',
+    value: 55454.24,
+    quantity: 65,
+    vendor: 'Kalunga',
     date: '12/01/2023',
   },
 ];
