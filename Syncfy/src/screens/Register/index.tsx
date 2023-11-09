fef4e553893ab73fa78e295bc7b9ae5f7f86624a
Gabriel-Triceri <112666478+Gabriel-Triceri@users.noreply.github.com>
2023-11-09 16:18:19 -0300

    feat: add validation to the registration page

--- Syncfy/src/screens/Register/index.tsx
+++ Syncfy/src/screens/Register/index.tsx
@@ -1,45 +1,54 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import CadastroForm from '../../components/RegisterForm';
-import {TouchableOpacity} from 'react-native';
-import {
-  Container,
-  ContentContainer,
-  BackgroundImage,
-  Logo,
-} from './style';
-import Toast from 'react-native-toast-message';
-import {StackNavigationProp} from '@react-navigation/stack';
-import {RootStackParamList} from '../../types/rock-stack-param-list';
+import { Container, ContentContainer, BackgroundImage, Logo } from './style';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { RootStackParamList } from '../../types/rock-stack-param-list';
+import { Alert } from 'react-native';
 
-type CadastroScreenNavigationProp = StackNavigationProp<
-  RootStackParamList,
-  'Cadastro'
->;
+
+type CadastroScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Cadastro'>;
 
 type Props = {
   navigation: CadastroScreenNavigationProp;
 };
 
-const Cadastro: React.FC<Props> = ({navigation}) => {
+const Cadastro: React.FC<Props> = ({ navigation }) => {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const [confirmPassword, setConfirmPassword] = useState<string>('');
+  const [cnpj, setCnpj] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(false);
 
   const handleCadastro = () => {
     if (loading) {
       return;
     }
-  };
 
-  const handleForgotPassword = () => {
-    // Lógica para esqueci minha senha
-    console.log('Usuário clicou em "Esqueci minha senha"');
-  };
+    // Validações
+    if (!email || !password || !confirmPassword || !cnpj) {
+      Alert.alert('Erro', 'Todos os campos são obrigatórios.');
+      return;
+    }
+  
+    if (!email.includes('@')) {
+      Alert.alert('Erro', 'O email deve conter @.');
+      return;
+    }
+  
+    if (cnpj.length !== 14) {
+      Alert.alert('Erro', 'O CNPJ deve ter 14 dígitos.');
+      return;
+    }
+  
+    if (password !== confirmPassword) {
+      Alert.alert('Erro', 'A senha e a confirmação de senha devem ser iguais.');
+      return;
+    }
 
+  };
   return (
     <Container>
-      <BackgroundImage
-        source={require('../../assets/images/white-background.png')}>
+      <BackgroundImage source={require('../../assets/images/white-background.png')}>
         <ContentContainer>
           <Logo source={require('../../assets/images/logo.png')} />
           <CadastroForm
@@ -47,13 +56,12 @@ const Cadastro: React.FC<Props> = ({navigation}) => {
             setEmail={setEmail}
             password={password}
             setPassword={setPassword}
-            onCadastroPress={handleCadastro} cnpj={''} setCnpj={function (text: string): void {
-              throw new Error('Function not implemented.');
-            } } confirmPassword={''} setConfirmPassword={function (text: string): void {
-              throw new Error('Function not implemented.');
-            } }          />
-          <TouchableOpacity onPress={handleForgotPassword}>
-          </TouchableOpacity>
+            confirmPassword={confirmPassword}
+            setConfirmPassword={setConfirmPassword}
+            cnpj={cnpj}
+            setCnpj={setCnpj}
+            onCadastroPress={handleCadastro}
+          />
         </ContentContainer>
       </BackgroundImage>
     </Container>
