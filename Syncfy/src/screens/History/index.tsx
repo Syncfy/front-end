8b997829bed2849209e90e3ce7e0965ad3006d48
shima_dev <104167280+shimarrudz@users.noreply.github.com>
2024-04-21 04:34:28 -0300

    remove: shopping cart from history page

--- Syncfy/src/screens/History/index.tsx
+++ Syncfy/src/screens/History/index.tsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import {ScrollView, View } from 'react-native';
 import {styles, historyData} from './style';
 import HistoryCard from '../../components/HistoryCard';
-import { IconContainer, SearchContainer, SearchIcon, SearchInput, ShoppingCartIcon, ShoppingCartIconContainer } from '../Portal/style';
+import { IconContainer, SearchContainer, SearchIcon, SearchInput } from '../Portal/style';
 
 const HistoryPage: React.FC = () => {
   const [searchQuery, setSearchQuery] = useState('');
@@ -26,16 +26,6 @@ const HistoryPage: React.FC = () => {
           clearButtonMode="while-editing"
         />
       </SearchContainer>
-      <ShoppingCartIconContainer>
-        <ShoppingCartIcon
-          source={require('../../assets/icons/shopping-cart-icon.png')}
-        />
-      </ShoppingCartIconContainer>
-      {/*  Título de histórico removido
-      <Image
-        source={require('../../assets/images/history-green-title.png')}
-        style={styles.titleImage}
-      /> */}
       <ScrollView style={styles.container}>
         {historyData.map((order, index) => (
           <HistoryCard key={index.toString()} order={order} />
