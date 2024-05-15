ba64616a806fe70fd5cc2e7e5622a922259469cf
shima_dev <104167280+shimarrudz@users.noreply.github.com>
2024-05-15 04:17:28 -0300

    feat: add styled import

--- Syncfy/src/screens/Result/style.tsx
+++ Syncfy/src/screens/Result/style.tsx
@@ -1,3 +1,5 @@
+import styled from 'styled-components/native';
+
 import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
@@ -54,3 +56,46 @@ export const styles = StyleSheet.create({
     color: 'gray',
   },
 });
+
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+`;
+
+export const SearchContainer = styled.View`
+  padding: 10px;
+  background-color: #52d974;
+  border-bottom-width: 1px;
+  border-color: #e4e4e4;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const SearchInput = styled.TextInput.attrs(() => ({
+  placeholderTextColor: 'gray',
+}))`
+  background-color: #ffffff;
+  padding-horizontal: 40px;
+  padding-vertical: 8px;
+  border-radius: 22px;
+  font-size: 16px;
+  flex: 1;
+  margin-right: 1px;
+  shadow-color: #000;
+  shadow-offset: 0px 2px;
+  shadow-opacity: 0.1;
+  shadow-radius: 2px;
+  elevation: 2;
+`;
+
+export const IconContainer = styled.View`
+  position: absolute;
+  left: 22px;
+  z-index: 1;
+`;
+
+export const SearchIcon = styled.Image`
+  width: 18px;
+  height: 18px;
+`;
